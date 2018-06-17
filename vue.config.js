fc2f9586316dde90d7387176b900732fe153a5f1
David <davidfallah1@gmail.com>
2018-06-17 12:27:02 +0100

    Configure demo to use custom HTML page

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   css: {
     extract: false
+  },
+  chainWebpack: config => {
+    config
+      .plugin("html")
+      .tap(args => {
+        if (process.env.DEMO) {
+          args[0].template = "./demo/public/index.html"
+        }
+        return args
+      })
   }
 }
